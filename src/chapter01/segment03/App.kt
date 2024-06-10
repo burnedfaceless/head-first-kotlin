41ccbf65172b950719bd1a4cdeb968e942fb3c62
Consolidated Utilities <hi@brianabbott.com>
2024-06-10 14:59:30 -0400

    created segment 3

---
+++ src/chapter01/segment03/App.kt
@@ -0,0 +1,12 @@
+package chapter01.segment03
+
+fun main() {
+    val x = 3
+    val y = 1
+    if (x > y) {
+        println("x is greater than y")
+    } else {
+        println("x is not greater than y")
+    }
+    println("This line runs no matter what")
+}
